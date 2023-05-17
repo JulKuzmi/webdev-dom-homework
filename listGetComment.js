237fd59b1c4881529f397a861fb8052b3bd4d68f
JulKuzmi <enchantress000@mail.ru>
2023-05-17 22:27:42 +0300

    hw 2.17

--- listGetComment.js
+++ listGetComment.js
@@ -1,8 +1,10 @@
 export const listGetComments = (comments, index) => {
+  const now = new Date();
+  const createDate = format(now, "MM-dd-yyyy hh:mm"); // 03-26-2023 10:33
     return `<li class="comment" data-index="${index}">
       <div class="comment-header">
         <div>${comments.author.name}</div>
-        <div>${comments.date}</div>
+        <div>${comments.createDate}</div>
       </div>
       <div class="comment-body">
         ${comments.isEdit ? `<textarea id="input" class="comment-text textarea" type="texrarea">${comments.text}</textarea>` : `<div class="comment-text">${comments.text.replaceAll("QUOTE_BEGIN", "<div class='quote'>").replaceAll("QUOTE_END", "</div>")}</div>`}
